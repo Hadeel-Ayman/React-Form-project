1e5d4884387597794136acad1fc49e9d42b515a1
Hadeel-Ayman <haddelayman4@gmail.com>
2023-01-15 07:33:52 +0300

    add some changes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,77 @@
-import SignUp from './pages/SignUp';
-import { LogInRoute } from './pages/LogIn';
-import React from 'react';
-import GameDay from './pages/GameDay';
-import UserList from './pages/UserList';
-import User from './pages/User';
-import {   Route , Routes, useNavigate   } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Plans from './pages/Plans'
-import Setting from './pages/Setting'
-import PageNotFound from './Components/PageNotFound';
-const AppWithUseNavegate =()=>{
-  const Navigate=useNavigate();
-  return <App Navigate={Navigate}/>
-}
+import SignUp from "./pages/SignUp";
+import { LogInRoute } from "./pages/LogIn";
+import React from "react";
+import GameDay from "./pages/GameDay";
+import UserList from "./pages/UserList";
+import User from "./pages/User";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Plans from "./pages/Plans";
+import Setting from "./pages/Setting";
+import PageNotFound from "./Components/PageNotFound";
+const AppWithUseNavegate = () => {
+  const Navigate = useNavigate();
+  return <App Navigate={Navigate} />;
+};
 class App extends React.Component {
-  state={
-    initialGmail:false,
+  state = {
+    initialGmail: false,
     isAuthorized: false,
-  }
-  byGmail = (v=false)=>{
-    this.setState({initialGmail:v})
-  }
-  
+  };
+  byGmail = (v = false) => {
+    this.setState({ initialGmail: v });
+  };
+
   componentDidMount() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) this.setState({ isAuthorized: true });
   }
 
   login = () => this.setState({ isAuthorized: true });
 
   logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
 
     this.setState({ isAuthorized: false });
-    this.props.Navigate("/")
+    this.props.Navigate("/");
   };
 
- render(){
-   return (
-    <>
-        {this.state.isAuthorized?(
+  render() {
+    return (
+      <>
+        {this.state.isAuthorized ? (
           <Routes>
-            <Route path="/" element={<Home logout={this.logout}/>}>
-              <Route index element={<GameDay />}/>
-              <Route path="Profile" element={<Profile/>}/>
-              <Route path="Users" element={<UserList/>}/>
-              <Route path="Users/:id" element={<User/>}/>
-              <Route path="Plans" element={<Plans/>}/>
-              <Route path="Setting" element={<Setting/>}/>
-              <Route path="*" element={<PageNotFound/>}/>
-            </Route> 
+            <Route path="/" element={<Home logout={this.logout} />}>
+              <Route index element={<GameDay />} />
+              <Route path="Profile" element={<Profile />} />
+              <Route path="Users" element={<UserList />} />
+              <Route path="Users/:id" element={<User />} />
+              <Route path="Plans" element={<Plans />} />
+              <Route path="Setting" element={<Setting />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Route>
           </Routes>
-        ):(
+        ) : (
           <Routes>
-            <Route path="/" element={<LogInRoute byGmail={this.byGmail} login={this.login} initialGmail={this.state.initialGmail}/>}/>
-            <Route path="/SignUp" element={<SignUp  byGmail={this.byGmail}/>}/>
-            <Route path="*" element={<PageNotFound/>}/>
+            <Route
+              path="/"
+              element={
+                <LogInRoute
+                  byGmail={this.byGmail}
+                  login={this.login}
+                  initialGmail={this.state.initialGmail}
+                />
+              }
+            />
+            <Route path="/SignUp" element={<SignUp byGmail={this.byGmail} />} />
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
-          )}
-    </>
- )
-}
+        )}
+      </>
+    );
+  }
 }
 
 export default AppWithUseNavegate;
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { object, string, boolean } from 'yup';
+import { object, string, boolean } from "yup";
 
 import "./style.css";
 import axios from "axios";
@@ -26,8 +26,9 @@ const defaults = {
   isWrongRepeat: false,
   passwordStrength: "",
   isLoading: false,
-}
-const passwordRegexStrong = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#*$%^&])(?=.{8,})/;
+};
+const passwordRegexStrong =
+  /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#*$%^&])(?=.{8,})/;
 const passwordRegexMedium = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.{6,})/;
 export default class SignUp extends Component {
   state = {
@@ -43,71 +44,77 @@ export default class SignUp extends Component {
     isLoading: false,
   };
   schema = object().shape({
-    name:
-      string()
-        .min(8, "Name should be more than 8 characters long")
-        .max(20)
-        .required("Name is required"),
-    email:
-      string()
-        .email("Invalid email")
-        .required("Email is required"),
-    password:
-      string()
-        .min(8, "Password must be at least 8 characters long")
-        .matches(passwordRegexStrong, "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character")
-        .required("Password is required"),
-    confirmPassword:
-      string()
-        .test('passwords-match', 'Passwords must match', () => {
-          const status = this.state.password === this.state.confirmPassword
-          this.setState({ isWrongRepeat: !status })
-          return status;
-        })
-        .required("Confirm password is required"),
-    isChecked:
-      boolean()
-        .oneOf([true], "You must agree to the terms and conditions")
-        .required("You must agree to the terms and conditions"),
+    name: string()
+      .min(8, "Name should be more than 8 characters long")
+      .max(20)
+      .required("Name is required"),
+    email: string().email("Invalid email").required("Email is required"),
+    password: string()
+      .min(8, "Password must be at least 8 characters long")
+      .matches(
+        passwordRegexStrong,
+        "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character"
+      )
+      .required("Password is required"),
+    confirmPassword: string()
+      .test("passwords-match", "Passwords must match", () => {
+        const status = this.state.password === this.state.confirmPassword;
+        this.setState({ isWrongRepeat: !status });
+        return status;
+      })
+      .required("Confirm password is required"),
+    isChecked: boolean()
+      .oneOf([true], "You must agree to the terms and conditions")
+      .required("You must agree to the terms and conditions"),
   });
 
   validate = (value) => {
     if (value.match(passwordRegexStrong)) {
-      this.setState({ passwordStrength: "strong" })
-    }
-    else if (value.match(passwordRegexMedium)) {
-      this.setState({ passwordStrength: "medium" })
+      this.setState({ passwordStrength: "strong" });
+    } else if (value.match(passwordRegexMedium)) {
+      this.setState({ passwordStrength: "medium" });
     } else {
-      this.setState({ passwordStrength: "weak" })
+      this.setState({ passwordStrength: "weak" });
     }
   };
   handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     this.setState({ isLoading: true });
     try {
-      await this.schema.validate({
-        name: this.state.name,
-        email: this.state.email,
-        password: this.state.password,
-        confirmPassword: this.state.confirmPassword,
-        isChecked: this.state.isChecked
-      }, { abortEarly: false });
+      await this.schema.validate(
+        {
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password,
+          confirmPassword: this.state.confirmPassword,
+          isChecked: this.state.isChecked,
+        },
+        { abortEarly: false }
+      );
       const res = await axios.post(`${API}/users/signup`, {
         name: this.state.name,
         email: this.state.email,
-        password: this.state.password
+        password: this.state.password,
       });
       if (res.data) {
-        this.setState((prevState) => ({ myData: { name: prevState.name, email: prevState.email, password: prevState.password },isSuccessfullyRegistered:true, ...defaults }));
+        this.setState((prevState) => ({
+          myData: {
+            name: prevState.name,
+            email: prevState.email,
+            password: prevState.password,
+          },
+          isSuccessfullyRegistered: true,
+          ...defaults,
+        }));
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
     this.setState({ isLoading: false });
   };
   handleChangeCheckBox = (e) => {
-    this.setState({ isChecked: e.target.checked })
-  }
+    this.setState({ isChecked: e.target.checked });
+  };
   handleChangeInput = (e) => {
     const { value, id } = e.target;
     this.setState({ [id]: value });
@@ -115,79 +122,88 @@ export default class SignUp extends Component {
       this.validate(value);
     }
   };
-  close = () => this.setState({ isSuccessfullyRegistered: false })
+  close = () => this.setState({ isSuccessfullyRegistered: false });
   render() {
     return (
-        <div className="mainbox signUp">
-          <div className={`imgBox ${this.props.class || ""}`}>
-            <Logo />
-            <img src={Dots} alt="" className="dots" />
-            <MainText color="#fff" />
-          </div>
-          <div className="form">
-            <Back />
-            <div className="content">
-              <FormText
-                H1="Register Individual Account!"
-                P="For the purpose of gamers regulation, your details are required."
-              />
-              <form onSubmit={this.handleSubmit}>
-                <Input
-                  onChange={this.handleChangeInput}
-                  id="name"
-                  type="text"
-                  placeholder="Enter email name"
-                  label="User Name*"
-                  value={this.state.name}
-                />
-                <Input
-                  onChange={this.handleChangeInput}
-                  id="email"
-                  type="email"
-                  placeholder="Enter email address"
-                  label="Email address*"
-                  value={this.state.email}
-                />
-                <Input
-                  onChange={this.handleChangeInput}
-                  id="password"
-                  type="password"
-                  placeholder="Password"
-                  label="Create password*"
-                  value={this.state.password}
-                />
-                <PasswordStrong passwordStrength={this.state.passwordStrength} />
-                <Input
-                  onChange={this.handleChangeInput}
-                  id="confirmPassword"
-                  type="password"
-                  placeholder="Repeat password"
-                  label="Repeat password*"
-                  value={this.state.confirmPassword}
-                  className={this.state.isWrongRepeat ? "wrong" : ""}
-                />
-                <Checkbox id="agree" label="I agree to terms & conditions" onChange={this.handleChangeCheckBox} />
-                <Button type="submit" bgColor="#1565D8" color="#fff">
-                  Register Account
-                </Button>
-                <Or />
-                <div className="LogInBtn">
-                  <img src={GoogleIcon} alt="" className="icon" />
-                  <Button
-                    type="button"
-                    bgColor="#fff"
-                    byGmail={this.props.byGmail}
-                    toLogin
-                    color="#000"
-                  >
-                    login
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </div>
-          {this.state.isSuccessfullyRegistered && <Alert Login={() => this.props.pageShow("LogIn")} close={this.close} />}
+      <div className="mainbox signUp">
+        <div className={`imgBox ${this.props.class || ""}`}>
+          <Logo />
+          <img src={Dots} alt="" className="dots" />
+          <MainText color="#fff" />
         </div>
+        <div className="form">
+          <Back />
+          <div className="content">
+            <FormText
+              H1="Register Individual Account!"
+              P="For the purpose of gamers regulation, your details are required."
+            />
+            <form onSubmit={this.handleSubmit}>
+              <Input
+                onChange={this.handleChangeInput}
+                id="name"
+                type="text"
+                placeholder="Enter email name"
+                label="User Name*"
+                value={this.state.name}
+              />
+              <Input
+                onChange={this.handleChangeInput}
+                id="email"
+                type="email"
+                placeholder="Enter email address"
+                label="Email address*"
+                value={this.state.email}
+              />
+              <Input
+                onChange={this.handleChangeInput}
+                id="password"
+                type="password"
+                placeholder="Password"
+                label="Create password*"
+                value={this.state.password}
+              />
+              <PasswordStrong passwordStrength={this.state.passwordStrength} />
+              <Input
+                onChange={this.handleChangeInput}
+                id="confirmPassword"
+                type="password"
+                placeholder="Repeat password"
+                label="Repeat password*"
+                value={this.state.confirmPassword}
+                className={this.state.isWrongRepeat ? "wrong" : ""}
+              />
+              <Checkbox
+                id="agree"
+                label="I agree to terms & conditions"
+                onChange={this.handleChangeCheckBox}
+              />
+              <Button type="submit" bgColor="#1565D8" color="#fff">
+                Register Account
+              </Button>
+              <Or />
+              <div className="LogInBtn">
+                <img src={GoogleIcon} alt="" className="icon" />
+                <Button
+                  type="button"
+                  bgColor="#fff"
+                  byGmail={this.props.byGmail}
+                  toLogin
+                  color="#000"
+                >
+                  login
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+        {this.state.isSuccessfullyRegistered && (
+          <Alert
+            Login={() => this.props.pageShow("LogIn")}
+            close={this.close}
+          />
+        )}
+      </div>
     );
   }
 }
